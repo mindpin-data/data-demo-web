7d6e4188410c9f91291a690f6b14321b557dc5a5
ben7th <ben7th@sina.com>
2017-05-15 17:41:19 +0800

    udpate db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 一带一路
+OborCountry.delete_all
 obor_countries = OborCountry.sample[:countries]
 obor_countries.each {|x|
   x[:now] = x[:now].to_json
@@ -17,26 +18,30 @@ obor_countries.each {|x|
 }
 
 # 全球销量
+CityAmount.delete_all
 CityAmount.sample[:cn_cities].each {|x|
   x[:in_china] = true
   CityAmount.create(x)
 }
-
 CityAmount.sample[:world_cities].each {|x|
   x[:in_china] = false
   CityAmount.create(x)
 }
 
+TotalAmount.delete_all
 TotalAmount.create(TotalAmount.sample)
 
+Material.delete_all
 Material.sample[:materials].each {|x|
   Material.create(x)
 }
 
+Scourge.delete_all
 Scourge.sample[:scourges].each {|x|
   Scourge.create(x)
 }
 
+Locality.delete_all
 Locality.sample[:localities].each {|x|
   Locality.create(x)
 }
