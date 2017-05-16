c8f205af2d3379d007adaf9ef9fc246d904fd1aa
ben7th <ben7th@sina.com>
2017-05-16 16:26:59 +0800

    add X-Frame-Options ALLOWALL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :add_xframe  
+  def add_xframe  
+    headers['X-Frame-Options'] = 'ALLOWALL'  
+  end  
 end
